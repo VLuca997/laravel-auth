30c6e938b7686b6bb2c7c20be43c9196b2670052
VLuca997 <kulertoast997@gmail.com>
2023-09-20 09:39:34 +0200

    creazione dashboard funzionante

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
+//import model
+use App\Http\Controllers\Admin\DashboardController;
+use Faker\Guesser\Name;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,9 +22,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function(){
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+});
+
+Route::middleware(['auth', 'verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group( function ()  { //Raggruppiamo le rotte da proteggere / prefisso admin
+        
+        Route::get('/dashboard' , [DashboardController::class, 'dashboard'])->name('dashboard');
+
+        //cosi le proteggerei tutte: ( di quella categoria.)
+        // Route::resource('posts' , PostController::class);
+});
+
+
+
+
+
 
 require __DIR__.'/auth.php';
